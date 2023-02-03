ece76550f80ff35d90aa1faf00b5085f402e78e5
dabuzida <dabuzida@gmail.com>
2023-02-03 17:56:32 +0900

    1

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,21 @@ class MyDateTime extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final DateTime now = DateTime.now();
-    final int xxx = now.millisecondsSinceEpoch;
 
-    final x = DateFormat('yyyy-MM-dd ahh:mm').format(DateTime.fromMillisecondsSinceEpoch(xxx, isUtc: false));
+    final y = now.add(const Duration(days: 330));
+
+    final int xxx = y.millisecondsSinceEpoch;
+
+    final x = DateFormat('G').format(DateTime.fromMillisecondsSinceEpoch(xxx, isUtc: false));
+    final x2 = DateFormat('yyyy-MM-dd ahh:mm').format(DateTime.fromMillisecondsSinceEpoch(xxx, isUtc: false));
+    final x3 = DateFormat('M/MM/MMM/MMMM/MMMMM/MMMMMM').format(DateTime.fromMillisecondsSinceEpoch(xxx, isUtc: false));
+    final x4 = DateFormat('L/LL/LLL/LLLL/LLLLL/LLLLLL').format(DateTime.fromMillisecondsSinceEpoch(xxx, isUtc: false));
+    final x5 = DateFormat('yyyy-MM-dd ahh:mm').format(DateTime.fromMillisecondsSinceEpoch(xxx, isUtc: false));
+    final x6 = DateFormat('yyyy-MM-dd ahh:mm').format(DateTime.fromMillisecondsSinceEpoch(xxx, isUtc: false));
+    final x7 = DateFormat('yyyy-MM-dd ahh:mm').format(DateTime.fromMillisecondsSinceEpoch(xxx, isUtc: false));
+    final x8 = DateFormat('yyyy-MM-dd ahh:mm').format(DateTime.fromMillisecondsSinceEpoch(xxx, isUtc: false));
+    final x9 = DateFormat('yyyy-MM-dd ahh:mm').format(DateTime.fromMillisecondsSinceEpoch(xxx, isUtc: false));
+    final x10 = DateFormat('yyyy-MM-dd ahh:mm').format(DateTime.fromMillisecondsSinceEpoch(xxx, isUtc: false));
 
     return Scaffold(
       appBar: AppBar(
@@ -35,10 +47,26 @@ class MyDateTime extends StatelessWidget {
         backgroundColor: Colors.teal,
         centerTitle: true,
       ),
-      body: Column(
-        children: <Widget>[
-          Text(x),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SizedBox(
+          height: 300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Text(x),
+              Text(x2),
+              Text(x3),
+              Text(x4),
+              Text(x5),
+              Text(x6),
+              Text(x7),
+              Text(x8),
+              Text(x9),
+              Text(x10),
+            ],
+          ),
+        ),
       ),
     );
   }
